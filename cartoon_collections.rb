d7606c0b4264f86dc88317d7049386c575784498
Savannah C <sgcorrell@gmail.com>
2019-03-04 22:31:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map! {|name| name.capitalize
+  array.map! {|name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
