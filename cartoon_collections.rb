a808407503100066a528279db31385e327808fa8
Savannah C <sgcorrell@gmail.com>
2019-03-04 22:26:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)
   array.each_with_index { |item, index| puts "#{index + 1}. #{item}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(# code an argument here)
   # Your code here
 end
 
