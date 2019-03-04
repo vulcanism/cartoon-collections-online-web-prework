855ab068aa62178ee4163135a17f2ee0bbfe10b0
Savannah C <sgcorrell@gmail.com>
2019-03-04 22:37:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(array)
   array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese(cheeses)
+def find_the_cheese(string)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheeses.detect {|
 end
